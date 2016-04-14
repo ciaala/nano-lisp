e2a3bbbdc7e58fc504cafaf6b29db936885cc08c
crypt <francesco.fiduccia@gmail.com>
2016-04-14 03:38:53 +0200

    The interpreter is narly ready

--- src/include/nanolisp.h
+++ src/include/nanolisp.h
@@ -9,6 +9,24 @@
 #include <unordered_map>
 namespace nl {
 
+    class nanolisp_runtime;
+
+    class nl_runtime : public nl_expression {
+    private:
+
+    public:
+        nl_runtime(string _id);
+
+        string id;
+
+        virtual nl_expression *run(nanolisp_runtime *runtime, vector<nl_expression *> arguments) = 0;
+
+        virtual ostream &print(ostream &os) override;
+
+        virtual bool isPrimitive() override;
+
+    };
+
     class nanolisp_runtime {
     private:
         unordered_map<string, nl_expression*> symbols;
@@ -16,11 +34,17 @@ namespace nl {
         nanolisp_runtime();
 
 
-        static inline nanolisp_runtime* create() {
-            return new nanolisp_runtime();
-        };
+        static nanolisp_runtime *create();
 
         void add(nl_runtime *runtime);
+
+        void add(string identifier, nl_expression *expression);
+
+        nl_expression *get(string identifier);
+
+        nl_expression *eval(nl_expression *expression);
+
+        void print_symbols();
     };
 
 
--- test/test_interpreter.cpp
+++ test/test_interpreter.cpp
@@ -8,17 +8,24 @@ using namespace std;
 bool
 check_interpreter(string &program, string &expected) {
     string result = nl::run_interpreter(program);
-    return ( result == expected );
+    if (result != expected) {
+        cout << "Interpretation has failed. Expected " << expected << " received " << result << endl;
+        return false;
+    } else {
+        return true;
+    }
 }
 
 int
 test_interpreter_main() {
     string program = "(def a 5)"
             "(def b 6)"
-            "(print (sum b c))";
+            "(print (sum a b))";
     string output = "11";
     if (check_interpreter(program, output) ){
         cout << program << " interpreted !!!";
+    } else {
+
     }
     return 0;
 }
